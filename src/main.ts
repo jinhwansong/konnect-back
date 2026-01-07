a6dbb50b9b23dab0580f1962aa910fa8aa056836
jinhwan <song7022556@gmail.com>
2026-01-08 04:06:17 +0900

    fix:origin수정

--- src/main.ts
+++ src/main.ts
@@ -24,10 +24,7 @@ async function bootstrap() {
 
   // HTTP CORS 설정
   app.enableCors({
-    origin: [
-      'https://konnect-front-phi.vercel.app',
-      'http://localhost:3000',
-    ],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie', 'Cookie'],
